0d11ca7c53d6f272d4abdc3e3cf44f608a65604a
Carmen Ortigoza <43401320+tifansun@users.noreply.github.com>
2026-03-07 16:22:15 -0600

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Abra esta pagina en [https://tifansun.github.io/minecraft-ovalo-extension/](https://tifansun.github.io/minecraft-ovalo-extension/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://minecraft.makecode.com/](https://minecraft.makecode.com/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/tifansun/minecraft-ovalo-extension** e importar
+
+## Edita este proyecto
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://minecraft.makecode.com/](https://minecraft.makecode.com/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/tifansun/minecraft-ovalo-extension** y haga clic en importar
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/minecraft
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,65 @@
+//% weight=100 color=#32A9D9 icon="◯"
+namespace figuras {
+
+    //% block="dibujar ovalo radio %radio con bloque %bloque"
+    export function ovalo(radio: number, bloque: number) {
+
+        let ubicacion = player.position()
+        let x = 0
+        let z = 0
+        let z2 = 0
+
+        for (let angulo2 = 0; angulo2 <= 180; angulo2++) {
+
+            x = ubicacion.getValue(Axis.X) + radio * Math.cos(angulo2 * 0.0174)
+            z = ubicacion.getValue(Axis.Z) - radio * Math.sin(angulo2 * 0.0174)
+            z2 = ubicacion.getValue(Axis.Z) + radio * Math.sin(angulo2 * 0.0174)
+
+            blocks.fill(
+                bloque,
+                world(x, ubicacion.getValue(Axis.Y), z),
+                positions.add(
+                    world(x, ubicacion.getValue(Axis.Y), z),
+                    pos(0, 3, 0)
+                ),
+                FillOperation.Replace
+            )
+
+            blocks.fill(
+                bloque,
+                world(x, ubicacion.getValue(Axis.Y), z2 + 70),
+                positions.add(
+                    world(x, ubicacion.getValue(Axis.Y), z2),
+                    pos(0, 3, 70)
+                ),
+                FillOperation.Replace
+            )
+        }
+
+        blocks.fill(
+            bloque,
+            positions.add(
+                ubicacion,
+                pos(-1 * radio, 0, 0)
+            ),
+            positions.add(
+                ubicacion,
+                pos(-1 * radio, 3, 70)
+            ),
+            FillOperation.Replace
+        )
+
+        blocks.fill(
+            bloque,
+            positions.add(
+                ubicacion,
+                pos(radio, 0, 0)
+            ),
+            positions.add(
+                ubicacion,
+                pos(radio, 3, 70)
+            ),
+            FillOperation.Replace
+        )
+    }
+}
