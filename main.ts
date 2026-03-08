98920d8e57c002fa5acc7f6cdcf980645426e6ee
Carmen Ortigoza <43401320+tifansun@users.noreply.github.com>
2026-03-07 21:07:33 -0600

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,5 @@
 //% weight=100 color=#32A9D9 icon="◎"
 namespace figuras {
-    /**
-     * Build an oval wall of specified ratio, length, and height out of block
-     * @param bloque the Minecraft block the wall will be built with
-     * @param radio how big is te oval
-     * @param largo how far a wall will span in blocks
-     * @param altura the number of blocks high the wall will stand
-     */
     //% block="dibujar ovalo con bloque $bloque radio $radio largo $largo altura $altura||ubicacion $ubicacion"
     //% bloque.shadow=minecraftBlock
     //% radio.defl=30
@@ -14,17 +7,20 @@ namespace figuras {
     //% altura.defl=3
     //% ubicacion.shadow=minecraftCreatePosition
     //% blockId="figurasOvalo" weight=100
-    export function ovalo(bloque: number, radio: number, largo: number, altura: number, ubicacion?: Position) {
-        let x = 0
-        let z = 0
-        let z2 = 0
+    export function ovalo(bloque: number, radio: number, largo: number, altura: number, ubicacion: Position) {
         if(!ubicacion){
             ubicacion = player.position()
         }
+        let x = 0
+        let z = 0
+        let z2 = 0
+
         for (let angulo2 = 0; angulo2 <= 180; angulo2++) {
+
             x = ubicacion.getValue(Axis.X) + radio * Math.cos(angulo2 * 0.0174)
             z = ubicacion.getValue(Axis.Z) - radio * Math.sin(angulo2 * 0.0174)
             z2 = ubicacion.getValue(Axis.Z) + radio * Math.sin(angulo2 * 0.0174)
+
             blocks.fill(
                 bloque,
                 world(x, ubicacion.getValue(Axis.Y), z),
@@ -34,6 +30,7 @@ namespace figuras {
                 ),
                 FillOperation.Replace
             )
+
             blocks.fill(
                 bloque,
                 world(x, ubicacion.getValue(Axis.Y), z2 + largo),
