c45607bbc147400dd6c2101bd01e4d5027527a7e
Carmen Ortigoza <43401320+tifansun@users.noreply.github.com>
2026-03-14 19:39:15 -0600

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 //% weight=100 color=#32A9D9 icon="◎"
 namespace figuras {
-    //% block="dibujar ovalo con bloque $bloque radio $radio"
+    //% block="dibujar ovalo con bloque $bloque radio $radio largo $largo alto $alto"
     //% bloque.shadow=minecraftBlock
     //% radio.defl=30
-    export function ovalo(bloque: number, radio: number) {
+    //% largo.defl=70
+    //% alto.defl=3
+    export function ovalo(bloque: number, radio: number, largo: number, alto: number) {
         let x = 0
         let z = 0
         let z2 = 0
@@ -19,17 +21,17 @@ namespace figuras {
                 world(x, ubicacion.getValue(Axis.Y), z),
                 positions.add(
                     world(x, ubicacion.getValue(Axis.Y), z),
-                    pos(0, 3, 0)
+                    pos(0, alto, 0)
                 ),
                 FillOperation.Replace
             )
 
             blocks.fill(
                 bloque,
-                world(x, ubicacion.getValue(Axis.Y), z2 + 70),
+                world(x, ubicacion.getValue(Axis.Y), z2 + largo),
                 positions.add(
                     world(x, ubicacion.getValue(Axis.Y), z2),
-                    pos(0, 3, 70)
+                    pos(0, alto, largo)
                 ),
                 FillOperation.Replace
             )
@@ -43,7 +45,7 @@ namespace figuras {
             ),
             positions.add(
                 ubicacion,
-                pos(-1 * radio, 3, 70)
+                pos(-1 * radio, alto, largo)
             ),
             FillOperation.Replace
         )
@@ -56,9 +58,38 @@ namespace figuras {
             ),
             positions.add(
                 ubicacion,
-                pos(radio, 3, 70)
+                pos(radio, alto, largo)
             ),
             FillOperation.Replace
         )
+    }
+    //% block="dibujar piso con bloque $bloque radio $radio largo $largo"
+    //% bloque.shadow=minecraftBlock
+    //% radio.defl=30
+    //% largo.defl=70
+    export function piso(bloque: number, radio: number, largo: number) {
+        let x = 0
+        let z = 0
+        let z2 = 0
+        let ubicacion = player.position()
+        for (let angulo2 = 0; angulo2 <= 180; angulo2++) {
+
+            x = ubicacion.getValue(Axis.X) + radio * Math.cos(angulo2 * 0.0174)
+            z = ubicacion.getValue(Axis.Z) - radio * Math.sin(angulo2 * 0.0174)
+            z2 = ubicacion.getValue(Axis.Z) + radio * Math.sin(angulo2 * 0.0174)
+
+            blocks.fill(
+                bloque,
+                positions.add(
+                    world(x, ubicacion.getValue(Axis.Y), z),
+                    pos(0, -1, 0)
+                ),
+                positions.add(
+                    world(x, ubicacion.getValue(Axis.Y), z2),
+                    pos(0, -1, largo)
+                ),
+                FillOperation.Replace
+            )
+        }
     }
 }
