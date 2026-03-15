6ea4e8071028cf103a89845f5e202cef7ceebb98
Carmen Ortigoza <43401320+tifansun@users.noreply.github.com>
2026-03-14 22:52:17 -0600

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -63,12 +63,11 @@ namespace figuras {
             FillOperation.Replace
         )
     }
-    //% block="dibujar piso con bloque $bloque radio $radio largo $largo posY $posY"
+    //% block="dibujar piso con bloque $bloque radio $radio largo $largo"
     //% bloque.shadow=minecraftBlock
     //% radio.defl=30
     //% largo.defl=70
-    //% posY.defl=-1
-    export function piso(bloque: number, radio: number, largo: number, posY:number) {
+    export function piso(bloque: number, radio: number, largo: number) {
         let x = 0
         let z = 0
         let z2 = 0
@@ -83,11 +82,42 @@ namespace figuras {
                 bloque,
                 positions.add(
                     world(x, ubicacion.getValue(Axis.Y), z),
-                    pos(0, posY, 0)
+                    pos(0, -1, 0)
                 ),
                 positions.add(
                     world(x, ubicacion.getValue(Axis.Y), z2),
-                    pos(0, posY, largo)
+                    pos(0, -1, largo)
+                ),
+                FillOperation.Replace
+            )
+        }
+    }
+    //% block="dibujar piso con bloque $bloque radio $radio largo $largo posY $posY"
+    //% bloque.shadow=minecraftBlock
+    //% radio.defl=30
+    //% largo.defl=70
+    //% posY1.defl=0
+    //% posY2.defl=3
+    export function relleno(bloque: number, radio: number, largo: number, posY1: number, posY2: number) {
+        let x = 0
+        let z = 0
+        let z2 = 0
+        let ubicacion = player.position()
+        for (let angulo2 = 0; angulo2 <= 180; angulo2++) {
+
+            x = ubicacion.getValue(Axis.X) + radio * Math.cos(angulo2 * 0.0174)
+            z = ubicacion.getValue(Axis.Z) - radio * Math.sin(angulo2 * 0.0174)
+            z2 = ubicacion.getValue(Axis.Z) + radio * Math.sin(angulo2 * 0.0174)
+
+            blocks.fill(
+                bloque,
+                positions.add(
+                    world(x, ubicacion.getValue(Axis.Y), z),
+                    pos(0, posY1, 1)
+                ),
+                positions.add(
+                    world(x, ubicacion.getValue(Axis.Y), z2),
+                    pos(0, posY2, largo-1)
                 ),
                 FillOperation.Replace
             )
